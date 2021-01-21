f9376db3a2a367d531b8d8b946975966df98e675
Oscar Andersson <oscarandersson2@gmail.com>
2021-01-21 16:57:18 +0100

    Skapat grunden för huvudmenyn

---
+++ Exercise2FlowControl/Menus.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise2FlowControl
+{
+    public class Menus
+    {
+        public static string GetMainMenu()
+        {
+            StringBuilder strBuilder = new StringBuilder("Huvudmeny");
+            strBuilder.Append(System.Environment.NewLine);
+
+            strBuilder.Append("(Navigera genom att ange siffra för önskat alternativ)");
+            strBuilder.Append(System.Environment.NewLine);
+
+            strBuilder.Append("0. Avsluta");
+            //strBuilder.Append(System.Environment.NewLine);
+            //strBuilder.Append(System.Environment.NewLine);
+
+            return strBuilder.ToString();
+        }
+    }
+}
--- Exercise2FlowControl/Program.cs
+++ Exercise2FlowControl/Program.cs
@@ -2,11 +2,60 @@
 
 namespace Exercise2FlowControl
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program program = new Program();
+            program.Run();
+
+            //Console.ReadLine();
+        }
+
+        private void Run()
+        {
+            //Console.WriteLine("Run program!");
+            bool bRun = true;
+
+            do
+            {
+                ShowMainMenu();
+                bRun = HandleMainMenuChoice();
+            } 
+            while (bRun);
+        }
+
+        private bool HandleMainMenuChoice()
+        {
+            bool bRun = true;
+
+            string strAction = Console.ReadLine();
+
+            if(strAction?.Length > 0)
+            {               
+                switch (strAction)
+                {
+                    case "0":
+                        bRun = false;
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Felaktig indata!");
+                        break;
+                }
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Felaktig indata!");
+            }
+
+            return bRun;
+        }
+
+        private void ShowMainMenu()
+        {
+            Console.WriteLine(Menus.GetMainMenu());
         }
     }
 }
